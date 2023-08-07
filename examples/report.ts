5bb338400261594e97c43942cd6e6151286f2412
Xubai Wang <18016038327@189.cn>
2023-08-08 06:43:16 +0800

    Add examples folder

---
+++ examples/report.ts
@@ -0,0 +1,21 @@
+#!/usr/bin/env -S deno run -A --unstable
+
+import {
+  alloc,
+  lib,
+  ref,
+  WGPUInstanceDescriptor,
+  WGPUGlobalReport
+} from "./mod.ts";
+
+// Create Instance
+const desc = alloc(WGPUInstanceDescriptor);
+desc.nextInChain = null;
+const instance = lib.symbols.wgpuCreateInstance(ref(desc));
+
+const report = alloc(WGPUGlobalReport);
+lib.symbols.wgpuGenerateReport(instance, ref(report));
+console.log(report);
+
+// Release instance
+lib.symbols.wgpuInstanceRelease(instance);
